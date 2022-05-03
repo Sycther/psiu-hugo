90302c9cc1ca9df1105c13883207e39e63ffce29
Sycther <Sycther@users.noreply.github.com>
2022-05-03 22:42:15 +0000

    deploy: 56d568c2d1e3080ded335fc271b5bdc8ea2bd56e

--- js/thumbnail_overlay.js
+++ js/thumbnail_overlay.js
@@ -14,7 +14,6 @@ function focusin(i) {
         popoverTriggerList[i].getElementsByClassName('card-img')[0].classList.add('overlay-img-dark');
         popoverTriggerList[i].getElementsByClassName('card-img-overlay')[0].classList.add('visible');
         popoverTriggerList[i].getElementsByClassName('card-img-overlay')[0].classList.remove('invisible');
-        console.log(i);
     };
 }
 
@@ -23,6 +22,5 @@ function focusout(i) {
         popoverTriggerList[i].getElementsByClassName('card-img')[0].classList.remove('overlay-img-dark');
         popoverTriggerList[i].getElementsByClassName('card-img-overlay')[0].classList.remove('visible');
         popoverTriggerList[i].getElementsByClassName('card-img-overlay')[0].classList.add('invisible');
-        console.log(-i);
     };
 }
